55b9afd5a62ec95a4b9b24a0d399d51251e4de6a
João Pedro Oliveira de Araujo <joaopedrooliveiradearaujo416@gmail.com>
2026-07-09 14:06:34 -0300

    chore: update project files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Navbar from "./Components/Navbar/Navbar"
-import BottomNavbar from "./Components/BottomNavbar/BottomNavbar"
+import Aside from './Components/Aside/Aside'
+import RegisterStore from './Components/RegisterStore/RegisterStore'
+import Login from './Components/RegisterUser/RegisterUser'
+import { useState } from "react"
 function App() {
+  const [asideOrNavbarItems, setAsideOrNavbarItems] = useState(1)
   return (
     <>
-      <Navbar />
-      <BottomNavbar />
+      <Navbar state={asideOrNavbarItems} setState={setAsideOrNavbarItems}/>
+      <Aside state={asideOrNavbarItems} setState={setAsideOrNavbarItems}/>
+      <RegisterStore state={asideOrNavbarItems} setState={setAsideOrNavbarItems}/>
+      <Login state={asideOrNavbarItems} setState={setAsideOrNavbarItems}/>
     </>
   )
 }
